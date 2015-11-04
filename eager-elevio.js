5c4341484255a385e7851866eee134d1d81a16f4
Chris Duell <me@chrisduell.com>
2015-11-04 12:00:45 +1100

    Allow for setting overrides

--- eager-elevio.js
+++ eager-elevio.js
@@ -1,7 +1,7 @@
-window.EagerSatisMeter = {
+window.EagerElevio = {
     init: function(options) {
 
-        var _elev = window._elev || {};
+        window._elev = window._elev || {};
         (function() {
             var i,e;
             i = document.createElement("script"),
@@ -12,14 +12,19 @@ window.EagerSatisMeter = {
                 e.parentNode.insertBefore(i,e);
         })();
 
-        _elev.account_id = options.token;//'konami';
-
-        var settings = {};
-        settings.token = options.token;
-        settings.preview = INSTALL_ID === 'preview';
-
+        window._elev.account_id = options.token || 'preview';
 
         var user;
+
+        if (options.docked_position) {
+            window._elev.docked_position = options.docked_position;
+        }
+        if (options.side) {
+            window._elev.side = options.side;
+        }
+        if (options.main_color) {
+            window._elev.main_color = options.main_color;
+        }
 
         if (options.automaticUser) {
             var emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
@@ -50,10 +55,10 @@ window.EagerSatisMeter = {
             } catch (e) {}
         }
 
-        user.via = 'eager';
 
         if (user && typeof user === 'object' && user.email) {
-            _elev.user = user;
+            user.via = 'eager';
+            window._elev.user = user;
         } else {
             return;
         }
